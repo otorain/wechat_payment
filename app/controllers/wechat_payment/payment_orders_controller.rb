2754e3da6a6f856f3999a913ea58d0977cce3fa5
ian <ianlynxk@gmail.com>
2022-01-19 15:48:42 +0800

    增加获取订单列表接口

--- app/controllers/wechat_payment/payment_orders_controller.rb
+++ app/controllers/wechat_payment/payment_orders_controller.rb
@@ -1,7 +1,9 @@
 
 class PaymentOrdersController < ApplicationController
   def index
+    @payment_orders = WechatPayment::PaymentOrder.by_state(params[:state])
 
+    render json: @payment_orders
   end
 
 end
